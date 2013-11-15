73a283a2dd28cd2410221d1e37c8c99260417c58
zinux <zinux@zinux-0S.(none)>
2013-11-15 11:24:29 +0700

    end filling layout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,32 +4,52 @@ describe "Static pages" do
 
   let(:base_title) { "RoR Sample App (Filling Layout)" }
   subject {page}
+
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before {visit root_path}
-    it {should have_content('Sample App')}
-    it {should have_title(full_title(''))}
-    it {should have_title('| Home')}
+    let(:heading)    { base_title }
+    let(:page_title) { '' }
+
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path}
-    it {should have_content('Help')}
-    it {have_title(full_title(''))}
-    it {have_title("| Help")}
+    let(:heading)    { base_title }
+    let(:page_title) { '' }
 
   end
 
   describe "About page" do
     before { visit about_path}
-    it {should have_content('About Us')}
-    it {should have_title(full_title(''))}
-    it {should have_title('| About Us')}
+    let(:heading)    { base_title }
+    let(:page_title) { '' }
   end
 
   describe "Contact Page" do
     before {visit contact_path}
-    it {should have_content('Contact')}
-    it {should have_title(full_title(''))}
-    it {should have_title('| Contact')}
+    let(:heading)    { base_title }
+    let(:page_title) { '' }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "home"
+    expect(page).to have_title(full_title(''))
+    click_link "Sign Up Now !"
+    expect(page).to have_title(full_title('Sign Up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
   end
 end
